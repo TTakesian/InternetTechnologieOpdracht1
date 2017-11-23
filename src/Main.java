273cdf5f646f79a03c7995b794f6fc20b8441b65
Tavet Takesian <indexviross@hotmail.com>
2017-11-23 10:29:03 +0100

    dwwd

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    void run(){
+        Opdracht1();
+        Opdracht2();
+        Opdracht3();
+    }
+
+    void Opdracht1(){
+
+    }
+
+    void Opdracht2(){
+
+    }
+
+    void Opdracht3(){
+
+    }
+}
