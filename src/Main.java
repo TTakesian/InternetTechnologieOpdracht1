2aefb31e760a9d38cbb2c90306b3ef1ac3c3ad61
swart\Govert <gdswa@outlook.com>
2017-11-30 12:09:15 +0100

    dwwd

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class Main {
     private Random random;
+
     public static void main(String[] args) {
         new Main().run();
     }
 
-    void run(){
+    void run() {
         ArrayList<Integer> algo1 = new ArrayList();
         algo1.add(5000);
         algo1.add(10000);
@@ -76,36 +77,46 @@ public class Main {
             System.out.println("Gemiddeld: "+(total/8) + "milliseconden");
         }
 */
-    Opdracht1(10);
+        Opdracht1(10);
     }
 
     /**
      * This function will create an array filled with random numbers, each number is unique and there are no doubles.
      * When generating a random number, the function loops through the array to check if that number already exist, if so
      * then it will repeat the process until there is a generated number that doesn't exist in the array
+     *
      * @param n The given array length
      */
-    long Opdracht1(int n){
+    long Opdracht1(int n) {
         int[] a = new int[n];
         int number;
+        boolean zeroUsed = false;
         //Start timer
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < a.length; i++) {
             number = (int) (Math.random() * ((n)));
-            for (int j = 0; j < a.length; j++) {
-                if(i==0){
-                    a[i] = number;
-                    break;
-                }else if (a[j] == number) {
-                    if(i!=0){
-                        i--;
+            boolean done = false;
+
+            while (!done) {
+                for (int j = 0; j < a.length; j++) {
+                    if (a[j] == number) {
+                        if (number == 0 && !zeroUsed) {
+                            done = true;
+                            zeroUsed = true;
+                            a[i] = number;
+                            j = a.length - 1;
+                        } else {
+                            j = 0;
+                            number = (int) (Math.random() * ((n)));
+                        }
+                    } else if (j == a.length - 1) {
+                        a[i] = number;
+                        done = true;
                     }
-                }else {
-                    a[i] = number;
-                    break;
                 }
             }
         }
+
 
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
@@ -113,7 +124,7 @@ public class Main {
         //Count the time spent
         long duration = (System.currentTimeMillis() - startTime);
         //Print out the duration of the whole process
-        System.out.println(duration+" milliseconden");
+        System.out.println(duration + " milliseconden");
         return duration;
     }
 
@@ -122,19 +133,20 @@ public class Main {
      * there are no dupplicates. There is an extra array that is used to check if the random generated number
      * already exists in the array, if so then it will try to generate a random number again and repeat the
      * same process until the random generated number is unique
+     *
      * @param n The given array length
      */
-    long Opdracht2(int n){
+    long Opdracht2(int n) {
         int[] list = new int[n];
         boolean[] used = new boolean[n];
         //Start the timer
         long startTime = System.currentTimeMillis();
-        for(int i = 0; i < n;i++){
+        for (int i = 0; i < n; i++) {
             boolean done = false;
-            while(!done) {
+            while (!done) {
                 int number = (int) (Math.random() * ((n)));
 
-                if(!used[number]){
+                if (!used[number]) {
                     used[number] = true;
                     list[i] = number;
                     done = true;
@@ -149,7 +161,7 @@ public class Main {
         //Count the time spent
         long duration = (System.currentTimeMillis() - startTime);
         //Print out the duration of the whole process
-        System.out.println(duration+" milliseconden");
+        System.out.println(duration + " milliseconden");
         return duration;
 
     }
@@ -158,9 +170,11 @@ public class Main {
      * This function will create an array and fill it with numbers, when adding each number
      * the added number will be swaped with another position where this position will be lower than its
      * index value and higher than the index value of 0(eg. we add 5, 5 will be swapped with a position between 0 and 5)
+     *
      * @param n The given array length
      */
-    long Opdracht3(int n){
+    long Opdracht3(int n) {
+        /*
         int[] list = new int[n];
         //Start the timer
         long startTime = System.currentTimeMillis();
@@ -185,6 +199,8 @@ public class Main {
         //Print out the duration of the whole process
         System.out.println(duration+"milliseconden");
         return duration;
-
+*/
+        return 2;
     }
+
 }
