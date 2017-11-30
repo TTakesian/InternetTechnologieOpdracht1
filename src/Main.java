4d693be5eab6b1837bf051115adec0a263c25258
swart\Govert <gdswa@outlook.com>
2017-11-30 10:12:42 +0100

    poop

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,21 @@ public class Main {
     }
 
     void run(){
-        ArrayList<Integer> algo1 = new ArrayList<>();
+        ArrayList<Integer> algo1 = new ArrayList();
         algo1.add(5000);
         algo1.add(10000);
         algo1.add(20000);
         algo1.add(50000);
         algo1.add(100000);
         algo1.add(200000);
-        ArrayList<Integer> algo2 = new ArrayList<>();
+        ArrayList<Integer> algo2 = new ArrayList();
         algo2.add(100000);
         algo2.add(200000);
         algo2.add(500000);
         algo2.add(1000000);
         algo2.add(5000000);
         algo2.add(10000000);
-        ArrayList<Integer> algo3 = new ArrayList<>();
+        ArrayList<Integer> algo3 = new ArrayList();
         algo3.add(2000000);
         algo3.add(5000000);
         algo3.add(10000000);
@@ -84,7 +84,7 @@ public class Main {
      * @param n The given array length
      */
     long Opdracht1(int n){
-        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> a = new ArrayList();
         int number;
         //Start timer
         long startTime = System.currentTimeMillis();
@@ -105,7 +105,7 @@ public class Main {
         //From milliseconds to seconds
         //duration = duration / 1000;
         //Print out the duration of the whole process
-        System.out.println(duration+"milli-seconds");
+        System.out.println(duration+" milliseconds");
         return duration;
     }
 
@@ -117,12 +117,12 @@ public class Main {
      * @param n The given array length
      */
     long Opdracht2(int n){
-        ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Boolean> used = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList();
+        ArrayList<Boolean> used = new ArrayList();
         for(int i = 0; i < n - 1; i++){
             used.add(false);
         }
-        System.out.println("Opdracht 2:");
+
         //Start the timer
         long startTime = System.currentTimeMillis();
         for(int i = 0; i < n - 1 ;i++){
@@ -147,7 +147,7 @@ public class Main {
         //From milliseconds to seconds
         //duration = duration / 1000;
         //Print out the duration of the whole process
-        System.out.println(duration+"milli-seconds");
+        System.out.println(duration+" milliseconds");
         return duration;
 
     }
@@ -159,7 +159,7 @@ public class Main {
      * @param n The given array length
      */
     long Opdracht3(int n){
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList();
         //Start the timer
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < n-1; i++) {
